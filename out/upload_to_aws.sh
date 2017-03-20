ae48747a1fe00882826b3ded9ae77ed1d7daf3fc
Sam Szreter <s.szreter@gmail.com>
2017-03-20 17:17:54 +0000

    Updates index layout and styles.

--- out/upload_to_aws.sh
+++ out/upload_to_aws.sh
@@ -1,1 +1,1 @@
-aws s3 sync . s3://kanjikai.com --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 sync . s3://www.kanjikai.com --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --exclude upload_to_aws.sh --exclude .DS_Store --exclude '*/.DS_Store'
